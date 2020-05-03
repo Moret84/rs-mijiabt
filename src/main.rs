cd0b09c1dd7765bde9e87cf7bdedcd78364cb78f
Aurélien Rivet <aurelienrivet@hotmail.fr>
2020-05-03 21:13:06 +0200

    introduce mijiabt service

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,19 @@
-mod btleplug_ble_repo;
-use btleplug_ble_repo::BtleplugBleRepo;
-
-mod dbus_ble;
-use dbus_ble::dbus_ble_repo::DbusBleRepo;
-
 mod api;
+mod dbus_ble;
+mod mijia_bt;
+
+use mijia_bt::MijiaBt;
 
 use std::thread;
 use std::time::Duration;
 
-const TARGET_DEVICE_ADDRESS: [u8; 6] = [0x4C, 0x65, 0xA8, 0xDB, 0xBD, 0x7D];
-const TARGET_SERVICE_UUID: &str = "0000fe95-0000-1000-8000-00805f9b34fb";
-const TARGET_DEVICE_NAME: &str = "MJ_HT_V1";
-
 fn main() {
-    dbus_test();
-//   let device = dbus_test();
-/*    if !device.is_empty() {
-        follow(device);
-    }*/
-    //btleplug_test();
-}
+    let mut mijia_bt = MijiaBt::new();
+    mijia_bt.set_on_data_updated_callback(Some(|mijiabt_data: (u16, u16)| {
+        println!("Temperature: {}, Humidity: {}", mijiabt_data.0, mijiabt_data.1);
+    }));
 
-fn btleplug_test() {
-    let mut ble = BtleplugBleRepo::new();
-
-    ble.set_device_filter(|address, name| {
-        name.contains(TARGET_DEVICE_NAME) || address.eq(&TARGET_DEVICE_ADDRESS)
-    });
-
-    let devices = ble.scan(10, true);
-
-    if devices.len() > 0 {
-        println!("{}", ble.connect(devices[0]));
-    }
-}
-
-fn dbus_test() {
-    let mut dbus_ble_repo = DbusBleRepo::new();
-    dbus_ble_repo.set_on_device_discovered_cb(|device| {
-        println!("{}", device.path);
-        println!("{}", device.local_name);
-        println!("{:#?}", device.service_data);
-    });
-
-    dbus_ble_repo.start_scan();
+    mijia_bt.start_listening();
 
     let mut i = 30;
     while i >= 0 {
@@ -52,5 +21,5 @@ fn dbus_test() {
         i = i -1;
     }
 
-    dbus_ble_repo.stop_scan();
+    mijia_bt.stop_listening();
 }
---
+++ src/mijia_bt.rs
@@ -0,0 +1,110 @@
+use crate::api::BleDevice;
+
+use crate::dbus_ble::dbus_ble_repo::DbusBleRepo;
+
+use std::sync::atomic::{AtomicU16, Ordering};
+use std::sync::{Arc, Mutex};
+
+const TARGET_DEVICE_NAME: &str = "MJ_HT_V1";
+const TARGET_SERVICE_UUID: &str = "0000fe95-0000-1000-8000-00805f9b34fb";
+
+pub struct MijiaBt {
+    ble_repo: DbusBleRepo,
+    current_data: Arc<(AtomicU16, AtomicU16)>,
+    on_data_updated: Arc<Mutex<Option<Box<dyn FnMut((u16, u16)) + Send + Sync + 'static>>>>,
+}
+
+impl MijiaBt {
+    /// Returns a new instance of the mijia_bt sensor abstraction.
+    pub fn new() -> MijiaBt {
+        let mijia_bt = MijiaBt {
+            ble_repo: DbusBleRepo::new(),
+            current_data: Arc::new((AtomicU16::new(0), AtomicU16::new(0))),
+            on_data_updated: Arc::new(Mutex::new(None))
+        };
+
+        mijia_bt
+    }
+
+    /// Start listening the mijia bt sensor.
+    pub fn start_listening(&self) {
+        self.ble_repo.start_scan();
+    }
+
+    /// Stop listening the mijia bt sensor.
+    pub fn stop_listening(&self) {
+        self.ble_repo.stop_scan();
+    }
+
+    /// Set the on data updated callback.
+    ///
+    /// # Arguments:
+    /// * `callback` - The callback to call when a mijia bt data update occurs
+    ///                The callback take a (temperature, humidity) u16 tuple as parameter.
+    pub fn set_on_data_updated_callback(&mut self, callback: Option<impl FnMut((u16, u16)) + Send + Sync + 'static>) {
+        match callback {
+            None => self.on_data_updated = Arc::new(Mutex::new(None)),
+            Some(callback) => {
+                self.on_data_updated = Arc::new(Mutex::new(Some(Box::new(callback))));
+
+                let on_advertisement_data = {
+                    let mijiabt_data_clone = self.current_data.clone();
+                    let on_data_updated_clone = self.on_data_updated.clone();
+                    move |device: &BleDevice| {
+                        if device.local_name == TARGET_DEVICE_NAME {
+                            if device.service_data.contains_key(TARGET_SERVICE_UUID) {
+                                let new_data = Self::parse_mijia_bt_data(&device.service_data[TARGET_SERVICE_UUID]);
+
+                                let mut data_changed = false;
+
+                                if mijiabt_data_clone.0.load(Ordering::Relaxed) != new_data.0 && new_data.0 != 0 {
+                                    mijiabt_data_clone.0.store(new_data.0, Ordering::Relaxed);
+                                    data_changed = true;
+                                }
+
+                                if mijiabt_data_clone.1.load(Ordering::Relaxed) != new_data.1  && new_data.1 != 0 {
+                                    mijiabt_data_clone.1.store(new_data.1, Ordering::Relaxed);
+                                    data_changed = true;
+                                }
+
+                                if data_changed {
+                                    let temperature = mijiabt_data_clone.0.load(Ordering::Relaxed);
+                                    let humidity = mijiabt_data_clone.1.load(Ordering::Relaxed);
+
+                                    if let Some(on_data_updated) = &mut *on_data_updated_clone.lock().unwrap() {
+                                        on_data_updated((temperature, humidity));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                };
+
+                self.ble_repo.set_on_advertisement_data_callback(on_advertisement_data);
+            }
+        }
+    }
+
+    /// Parse mijia btd advertisement data into temperature and humidity.
+    ///
+    /// # Arguments:
+    /// * `input` - The input raw data.
+    ///
+    /// Returns a (temperature, humidity) u16 tuple.
+    fn parse_mijia_bt_data(input: &Vec<u8>) -> (u16, u16) {
+        let mut temperature: u16 = 0;
+        let mut humidity : u16 = 0;
+        if input.len() == 18 {
+            temperature = ((input[15] as u16) << 8) | input[14] as u16;
+            humidity = ((input[17] as u16) << 8) | input[16] as u16;
+        } else if input.len() == 16 {
+            if input[11] == 6 {
+                humidity = ((input[15] as u16) << 8) | input[14] as u16;
+            } else if input[11] == 4 {
+                temperature = ((input[15] as u16) << 8) | input[14] as u16;
+            }
+        }
+
+        (temperature, humidity)
+    }
+}
